8b0869d25c38026e7f7e5f07834f431e66b49101
Likhitha A C <likhitha.ac@mitratech.com>
2023-02-10 12:38:37 +0530

    Revert "Commit3"
    This reverts commit 516bfb6580e29293720ca82785a2d285f678548f.

--- src/test/java/Practice/GetAllthevaluesofadynamicwebtable.java
+++ src/test/java/Practice/GetAllthevaluesofadynamicwebtable.java
@@ -48,9 +48,7 @@ list.add(12);
 
 		System.out.println("Commit-1");
 		System.out.println("Commit-2");
-		System.out.println("Commit-3");
 		System.out.println("Commit-6");
-
 		System.out.println("Commit-7");
 		System.out.println("Commit-8");
 >>>>>>> Likhitha-Thirdfeaturebranch
