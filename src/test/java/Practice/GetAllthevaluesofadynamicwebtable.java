864aeb8b93200475f4013eed090ce0d7099df58c
Likhitha A C <likhitha.ac@mitratech.com>
2023-02-10 12:06:26 +0530

    commit4

--- src/test/java/Practice/GetAllthevaluesofadynamicwebtable.java
+++ src/test/java/Practice/GetAllthevaluesofadynamicwebtable.java
@@ -49,6 +49,7 @@ list.add(12);
 		System.out.println("Commit-1");
 		System.out.println("Commit-2");
 		System.out.println("Commit-3");
+		System.out.println("Commit-4");
 >>>>>>> Likhitha-Thirdfeaturebranch
 	}
 
